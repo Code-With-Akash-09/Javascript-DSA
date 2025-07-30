ae7d5070b22146daef09bd8f7d1b724863cea827
Akash09082001 <aakashl@letsupgrade.in>
2025-07-30 16:29:19 +0530

    add: palindrome learnd

---
+++ warm-up/palindrome/main.js
@@ -0,0 +1,21 @@
+// Palindrome
+
+
+const isPalindrome = (x) => {
+    if (x < 0) return false
+
+    let xCopy = x
+    let rev = 0
+
+    while (x > 0) {
+        let rem = x % 10
+        rev = (10 * rev) + rem
+        x = Math.floor(x / 10)
+    }
+
+    return rev == xCopy
+}
+
+let nums = 898
+let result = isPalindrome(nums)
+console.log(result);
