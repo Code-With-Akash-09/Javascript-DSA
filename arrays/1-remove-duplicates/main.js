470df78a4829a626fb15a63e0994d9fd1064efb1
Akash09082001 <aakashl@letsupgrade.in>
2025-08-02 17:03:15 +0530

    fix: changes uncommented

--- arrays/1-remove-duplicates/main.js
+++ arrays/1-remove-duplicates/main.js
@@ -6,10 +6,10 @@ const removeDuplicates = (nums) => {
     let x = 0;
 
     for (let i = 0; i < nums.length; i++) {
-        // if (nums[i] > nums[x]) {
-        //     x = x + 1
-        //     nums[x] = nums[i]
-        // }
+        if (nums[i] > nums[x]) {
+            x = x + 1
+            nums[x] = nums[i]
+        }
 
     }
     return x + 1
